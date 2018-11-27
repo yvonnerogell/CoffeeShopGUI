ce393f57ed60d84d67453dd1e302018eb74ff169
yvonnerogell <yvonnerogell@users.noreply.github.com>
2018-11-26 18:12:17 -0800

    Added one query

--- CoffeeShopGUI/src/queryrunner/QueryJDBC.java
+++ CoffeeShopGUI/src/queryrunner/QueryJDBC.java
@@ -8,7 +8,11 @@ import java.sql.Connection;
 import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+//import java.sql.ResultSet;
+//import java.sql.ResultSetMetaData;
+//import java.sql.PreparedStatement;
 import java.util.Scanner;
+
 import java.util.ArrayList;
 /**
  *
